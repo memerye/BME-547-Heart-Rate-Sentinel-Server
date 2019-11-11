9c1143d953cdad79f41c0b833ba29b84b10f86c4
memerye <lx66@duke.edu>
2019-11-11 10:49:01 -0500

    add heart rate post

--- test_hr_server.py
+++ test_hr_server.py
@@ -21,10 +21,16 @@ def test_validate_patient_keys(patient_info, expected):
 
 
 @pytest.mark.parametrize("patient_info, expected", [
+    ({"patient_id": 1,
+      "attending_email": "dr_user_id@yourdomain.com",
+      "patient_age": 50}, 1),
     ({"patient_id": "1",
       "attending_email": "dr_user_id@yourdomain.com",
       "patient_age": 50}, 1),
     ({"patient_id": "nan",
+      "attending_email": "dr_user_id@yourdomain.com",
+      "patient_age": 50}, False),
+    ({"patient_id": "1.5",
       "attending_email": "dr_user_id@yourdomain.com",
       "patient_age": 50}, False),
     ({"patient_id": "A1",
@@ -63,9 +69,47 @@ def test_validate_patient_email(patient_info, expected):
       "patient_age": "50"}, 50),
     ({"patient_id": "1",
       "attending_email": "dr_user_id@yourdomain.com",
+      "patient_age": "nan"}, False),
+    ({"patient_id": "1",
+      "attending_email": "dr_user_id@yourdomain.com",
+      "patient_age": "35.5"}, False),
+    ({"patient_id": "1",
+      "attending_email": "dr_user_id@yourdomain.com",
       "patient_age": "a50"}, False)
 ])
 def test_validate_patient_age(patient_info, expected):
     from hr_server import validate_patient_age
     result = validate_patient_age(patient_info)
+    assert result == expected
+
+
+@pytest.mark.parametrize("patient_hr, expected", [
+    ({"patient_id": "1",
+      "heart_rate": 100}, True),
+    ({"patientid": "1",
+      "heart_rate": 100}, False),
+    ({"patient_id": "1",
+      "heart_rat": 100}, False)
+])
+def test_validate_hr_keys(patient_hr, expected):
+    from hr_server import validate_hr_keys
+    result = validate_hr_keys(patient_hr)
+    assert result == expected
+
+
+@pytest.mark.parametrize("patient_hr, expected", [
+    ({"patient_id": "1",
+      "heart_rate": 100}, 100),
+    ({"patient_id": "1",
+      "heart_rate": "100"}, 100),
+    ({"patient_id": "1",
+      "heart_rate": "nan"}, False),
+    ({"patient_id": "1",
+      "heart_rate": "60.5"}, False),
+    ({"patient_id": "1",
+      "heart_rate": "a100"}, False)
+])
+def test_validate_hr(patient_hr, expected):
+    from hr_server import validate_hr
+    result = validate_hr(patient_hr)
     assert result == expected
